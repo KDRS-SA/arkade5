e3d702c430bfddcba4066c4cc16535b53886d715
Jørgen Tellnes <jorgen@arkitektum.no>
2020-03-11 17:32:42 +0100

    Add all relevant files from .NET framework based GUI project

--- src/Arkivverket.Arkade.GUI/App.xaml.cs
+++ src/Arkivverket.Arkade.GUI/App.xaml.cs
@@ -1,17 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
+using Arkivverket.Arkade.Core.Base;
+using Arkivverket.Arkade.GUI.Properties;
+using Arkivverket.Arkade.GUI.Util;
+using Arkivverket.Arkade.Core.Util;
+using Serilog;
 
 namespace Arkivverket.Arkade.GUI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private static ILogger _log;
+
+        public App()
+        {
+            try
+            {
+                ArkadeProcessingArea.Establish(Settings.Default.ArkadeProcessingAreaLocation);
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine("Exception while establishing arkade processing area: " + e.Message);
+            }
+
+            LogConfiguration.ConfigureSeriLog();
+            _log = Log.ForContext<App>();
+            // For some reason this will not work for exceptions thrown from inside the Views.
+            AppDomain.CurrentDomain.UnhandledException += MyHandler;
+        }
+
+        public static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            var e = (Exception) args.ExceptionObject;
+            new DetailedExceptionMessage(e).ShowMessageBox();
+            _log.Error("Unexpected exception", e);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _log.Information("Arkade " + ArkadeVersion.Current + " started");
+
+            base.OnStartup(e);
+            var bs = new Bootstrapper();
+            bs.Run();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _log.Information("Arkade " + ArkadeVersion.Current + " stopping");
+
+            if (!ArkadeProcessingAreaLocationSetting.IsApplied())
+                ArkadeProcessingArea.Destroy();
+
+            else if (ArkadeInstance.IsOnlyInstance)
+                ArkadeProcessingArea.CleanUp();
+
+            base.OnExit(e);
+        }
     }
 }
